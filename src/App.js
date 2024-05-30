efb224e9964ab51d38820c4a953080bdecb66989
TejaswiniKakarlamudi <kakarlamuditejaswini@gmail.com>
2024-05-30 18:15:51 +0530

    Tooltip buit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
 import Cards from './Components/Card/Card';
-import img from './Assets/vibrating-headphone 1.svg';
+
 function App() {
   const data = {
     songs: ['ureka', 'sakai', 'mikai', ],
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -9,18 +9,18 @@ function Cards({ type, data }) {
       case 'album':{
         const {slug, songs, image,follows,title} = data;
         return (
-          <Card className={styles.card} sx={{ backgroundColor: 'var(--color-black)'}}>
+          <Card className={styles.card} sx={{ bgcolor: 'var(--color-black)'}}>
           <Tooltip title={`${songs.length} songs`} placement="top" arrow >
-            <a href={`/album/${slug}`} sx={{ width:'159px', height:'232px' }}>
+            <a href={`/album/${slug}`}>
              
-                <div className={styles.imageContainer} sx={{ width:'159px', height:'205px',borderRadius: '10px' }}>
+                <div className={styles.imageContainer} style={{ width:'159px', height:'205px', borderRadius: '10px' }}>
                   <CardMedia
                     component="img"
                     alt="Vibrating Headphones"
                     image={image}
                     className={styles.cardImage}
                   />
-                  <div className={styles.cardChip} sx={{ backgroundColor:'var(--color-white)',borderRadius: '10px'}}>
+                  <div className={styles.cardChip} style={{ backgroundColor: 'var(--color-white)'}}>
                     <Chip label={`${follows} Follows`} className={styles.chip} sx={{ backgroundColor: 'var(--color-black)', color: 'var(--color-white)', height:'22px' }} />
                   </div>
                 </div>
@@ -39,20 +39,20 @@ function Cards({ type, data }) {
         const {image,likes,title,slug} = data;
         return(
           <a href={`/album/${slug}`}>
-          <Card className={styles.card}>
-            <div className={styles.imageContainer}>
+          <Card className={styles.card} sx={{ bgcolor: 'var(--color-black)'}}>
+            <div className={styles.imageContainer} style={{ width:'159px', height:'205px',borderRadius: '10px' }}>
               <CardMedia
                 component="img"
                 alt="Vibrating Headphones"
                 image={image}
                 className={styles.cardImage}
               />
-              <div className={styles.cardChip} sx={{ backgroundColor:  'var(--color-white)' }}>
+              <div className={styles.cardChip} style={{ bgcolor: 'var(--color-black)'}}>
                 <Chip label={`${likes} Likes`} className={styles.chip} sx={{ backgroundColor: 'var(--color-black)', color: 'var(--color-white)', height:'22px' }} />
               </div>
             </div>
-            <CardContent className={styles.cardText}>
-              <Typography gutterBottom variant="h6" component="div">
+            <CardContent className={styles.cardText}sx={{ width:'159px', height:'30px', bgcolor:'var(--color-black)', color:'var(--color-white)', textAlign:'left', }}>
+                  <Typography gutterBottom variant="p" component="div">
                 {title}
               </Typography>
             </CardContent>
